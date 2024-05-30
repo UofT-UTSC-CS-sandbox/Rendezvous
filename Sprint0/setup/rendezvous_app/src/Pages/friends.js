d61ef8cfad8d7ddc642f47ee1ee2c7d2ff587537
Andy <mazhiyuannn@gmail.com>
2024-05-29 20:32:14 -0400

    added setup for frontend

---
+++ Sprint0/setup/rendezvous_app/src/Pages/friends.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Friends = () => {
+    return (
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "centre",
+                alignItems: "centre",
+                height: "100vh",
+            }}
+        >
+            <h1>Friends List</h1>
+        </div>
+    );
+};
+
+export default Friends;
