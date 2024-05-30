c289c2ad700bfef884fc1d7e92787aae574d1ca1
Andy <mazhiyuannn@gmail.com>
2024-05-29 20:32:14 -0400

    added setup for frontend

---
+++ Sprint0/setup/rendezvous_app/src/App.js
@@ -0,0 +1,37 @@
+//import logo from './logo.svg';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from "react-router-dom";
+import Home from "./Pages";
+import About from "./Pages/about";
+import Events from "./Pages/events";
+import Friends from "./Pages/friends";
+import SignUp from "./Pages/signup";
+
+function App() {
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/home" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route
+                    path="/events"
+                    element={<Events />}
+                />
+                <Route path="/friends" element={<Friends />} />
+                <Route
+                    path="/sign-up"
+                    element={<SignUp />}
+                />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
