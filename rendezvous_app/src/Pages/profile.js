d2d72defa34d73d55cfdb4981456f0769876a17d
Andy <mazhiyuannn@gmail.com>
2024-06-09 16:23:24 -0400

    can change profile picture

--- rendezvous_app/src/Pages/profile.js
+++ rendezvous_app/src/Pages/profile.js
@@ -1,25 +1,67 @@
-import React, {useState, useEffect} from "react"; 
+import React, {useState, useEffect, useRef} from "react"; 
 import "./CSS/profile.css"
 import pfp from "./images/stickmanpfp.png";
 import LeftSideBar from "../components/Sidebars/LeftSideBar";
 import RightSideBar from "../components/Sidebars/RightSideBar";
 
 const Profile = () => {
+    //bio handler
     const [bio, setBio] = useState(() => {
         return localStorage.getItem("bio") ||
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel ornare augue, vel gravida leo. Nam nec mi sit amet dui venenatis pulvinar.";});
-    const handleEditBiography = () => {
+    const handleEditBio = () => {
         const newBio = prompt("Enter your new bio:");
         if (newBio !== null) {
             setBio(newBio);
             localStorage.setItem("bio", newBio);
         }
     };
+    
+    //profile picture handler
+    const [profilePic, setProfilePic] = useState(() => {
+        return localStorage.getItem("profilePic") || pfp;
+    });
+    const [fileInputDisabled, setFileInputDisabled] = useState(false);
+    const imageFileInputRef = useRef(null);
+    const handleProfilePicChange = () => {
+        setFileInputDisabled(true);
+        imageFileInputRef.current.click();
+    }
+    //for some reason the button won't work unless I do these 2 functions
+    const handleFileInputChange = () => {
+        const imageFile = imageFileInputRef.current.files[0];
+        if (imageFile){
+            const reader = new FileReader();
+            reader.onload = () => {
+                const imageDataUrl = reader.result;
+                setProfilePic(imageDataUrl);
+                localStorage.setItem("profilePic", imageDataUrl);
+                setFileInputDisabled(false);
+            };
+            reader.readAsDataURL(imageFile);
+        }
+    };
+    // const handleProfilePicChange = () => {
+    //     const imageFile = imageFileInputRef.current.files[0];
+    //     if (imageFile){
+    //         const reader = new FileReader();
+    //         reader.onload = () => {
+    //             const imageDataUrl = reader.result;
+    //             setProfilePic(imageDataUrl);
+    //             localStorage.setItem("profilePic", imageDataUrl);
+    //         };
+    //         reader.readAsDataURL(imageFile);
+    //     }
+    // };
 
     useEffect(() => {
         localStorage.setItem("bio", bio);
     }, [bio]);
     
+    useEffect(() => {
+        localStorage.setItem("profilePic", profilePic);
+    }, [profilePic]);
+
     return (
         <>
             <div>
@@ -34,7 +76,14 @@ const Profile = () => {
                 }}
             >
                 <h1>Your Profile</h1>
-                <img src={pfp} alt="Profile pic" className="resized-image"/>;
+                <img src={profilePic} alt="Profile pic" className="resized-image"/>
+                <input
+                    type="file"
+                    accept="image/*"
+                    ref={imageFileInputRef}
+                    style={{ display: "none" }}
+                    onChange={handleFileInputChange}
+                />
             </div>
             <div
                 style={{
@@ -45,7 +94,11 @@ const Profile = () => {
                     marginRight: "25%",
                 }}
             >
-                <button className="button" onClick={handleEditBiography}>Edit Bio</button>
+                <div style={{ display: "flex", alignItems: "center" }}>
+                    <button className="button" onClick={handleProfilePicChange} disabled={fileInputDisabled}>Change Profile Pic</button>
+                    <button className="button" onClick={handleEditBio}>Edit Bio</button>
+                </div>
+
                 <h2>{bio}</h2>
             </div>
         </>
