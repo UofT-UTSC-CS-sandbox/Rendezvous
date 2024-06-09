70b1585c4c21b8e2b77e5b351c0c640e6b3232c7
shijunsui <shijun.sui@mail.utoronto.ca>
2024-06-09 17:33:26 -0400

    modify Friend page with friend list

--- rendezvous_app/src/Pages/friends.js
+++ rendezvous_app/src/Pages/friends.js
@@ -1,16 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Friends = () => {
+    const [sidebarVisible, setSidebarVisible] = useState(true);
+
+    const friendbtn = {
+        margin: "10px",
+        padding: "10px",
+        backgroundColor: "#007BFF",
+        color: "white",
+        border: "none",
+        borderRadius: "5px",
+        cursor: "pointer",
+    };
+
+    const toggleSidebarVisibility = () => {
+        setSidebarVisible(!sidebarVisible);
+    };
+
     return (
         <div
             style={{
                 display: "flex",
-                justifyContent: "centre",
-                alignItems: "centre",
                 height: "100vh",
             }}
         >
-            <h1>Friends List</h1>
+            <div
+                style={{
+                    flex: 1,
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+                <h1>Friends List</h1> <br></br>
+                <div>
+                <button onClick={toggleSidebarVisibility} style={{ margin: "20px" }}>
+                    {sidebarVisible ? "Hide my Friend List" : "Show my Friend List"}
+                </button>
+                </div>
+            </div>
+            {sidebarVisible && (
+                <div
+                    style={{
+                        width: "200px",
+                        backgroundColor: "#f0f0f0",
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        padding: "10px",
+                    }}
+                >
+                <div style={{ marginBottom: "20px", textAlign: "center" }}>
+                    <img
+                        src="https://www.w3schools.com/w3images/avatar2.png" 
+                        alt="Avatar"
+                        style={{ width: "80px", height: "80px", borderRadius: "50%" }}
+                    />
+                    <p style={{ margin: "5px 0", fontSize: "16px", color:"#000" }}>User Name</p>
+                    </div>
+                    <button style={friendbtn}>Friend 1</button>
+                    <button style={friendbtn}>Friend 2</button>
+                    <button style={friendbtn}>Friend 3</button>
+                    <button style={friendbtn}>Friend 4</button>
+                    <button style={friendbtn}>Friend 5</button>
+                </div>
+            )}
         </div>
     );
 };
