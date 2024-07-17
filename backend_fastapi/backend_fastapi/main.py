d06dca82a612ac3bc015cb363b2e975ba4c0e27a
SeanFridkin <sean.fridkin@mail.utoronto.ca>
2024-07-17 08:13:38 -0400

    Backend Draft for friend feature. Comments to be added before Friday

--- backend_fastapi/backend_fastapi/main.py
+++ backend_fastapi/backend_fastapi/main.py
@@ -36,6 +36,13 @@ Account_to_account = Table(
     Column("right_friend_id", Integer, ForeignKey("accounts.id"), primary_key=True),
 )
 
+friend_request_table = Table(
+    "friend_request_table",
+    Base.metadata,
+    Column("friend_id", Integer, ForeignKey("accounts.id"), primary_key=True),
+    Column("right_friend_id", Integer, ForeignKey("accounts.id"), primary_key=True),
+)
+
 class FriendCompressedProfile(BaseModel):
     username: str
     profile_image_src: str
@@ -66,6 +73,21 @@ class Account(Base):
         primaryjoin= (id == Account_to_account.c.right_friend_id),
         secondaryjoin= (id == Account_to_account.c.friend_id),
         back_populates="right_friends",
+    )
+
+     right_friend_requests: Mapped[List["Account"]] = relationship(
+        "Account",
+        secondary=friend_request_table,
+        primaryjoin= (id == friend_request_table.c.friend_id),
+        secondaryjoin= (id == friend_request_table.c.right_friend_id),
+        back_populates="friend_requests",
+    )
+    friend_requests: Mapped[List["Account"]] = relationship(
+        "Account",
+        secondary=friend_request_table,
+        primaryjoin= (id == friend_request_table.c.right_friend_id),
+        secondaryjoin= (id == friend_request_table.c.friend_id),
+        back_populates="right_friend_requests",
     )
 
     def __repr__(self) -> str:
@@ -201,28 +223,65 @@ def register(user: UserIn, db: Session = Depends(get_db)):
         db.commit()
         return JSONResponse(content={"message": "You have successfully registered!"})
     
-""" Adds the (first) user with username=username as
-    a friend of the current_user.
+""" Sends new friend request to the user with matching username.
+
+    If the friend_user has already sent a friend request to the
+    calling user, the users are added as friends.
+    Otherwise, sends a friend request.
 
     Throws HTTPException if:
-        - user does not exist
+        - friend does not exist
         - authentication for current user fails
         - current user is attempting to add themselves as a friend
-        - user is already friends with the current user. 
-    """
-@app.post("/addfriend")
-def add_friend (friend_user: Account = Depends(get_user_from_usernameIn),
+        - friend is already friends with the current user.
+        - the current user has already sent a friend request to friend. """
+@app.post("/sendfriendrequest")
+def send_friend_request (friend_user: Account = Depends(get_user_from_usernameIn),
                 current_user: Account = Depends(get_current_user),
                 db: Session = Depends(get_db)):
     if friend_user == current_user:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="You cannot friend yourself.") 
     if friend_user in current_user.friends:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User is already a friend.") 
-
-    friend_user.friends.append(current_user)
-    current_user.friends.append(friend_user)
+    if friend_user in current_user.friend_requests:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Friend request to user already sent.") 
+    # friend has already sent a friend request to the current user
+    if friend_user in current_user.right_friend_requests:
+        friend_user.friend_requests.remove(current_user)
+        friend_user.friends.append(current_user)
+        current_user.friends.append(friend_user)
+        db.commit()
+        return JSONResponse(content={"message": "Friend successfully added!"})
+    # creates new friend request
+    current_user.friend_requests.append(friend_user)
     db.commit()
-    return JSONResponse(content={"message": "Friend successfully added!"})
+    return JSONResponse(content={"message": "Friend request sent!"})
+
+def deny_friend_request (friend_user: Account = Depends(get_user_from_usernameIn),
+                current_user: Account = Depends(get_current_user),
+                db: Session = Depends(get_db)):
+    current_user.right_friend_requests.remove(friend_user)
+    db.commit()
+    return JSONResponse(content={"message": "Friend request denied."})
+
+def remove_friend (friend_user: Account = Depends(get_user_from_usernameIn),
+                current_user: Account = Depends(get_current_user),
+                db: Session = Depends(get_db)):
+    current_user.friends.remove(friend_user)
+    friend_user.friends.remove(current_user)
+    db.commit()
+    return JSONResponse(content={"message": "Friend removed."})
+    
+
+""" Adds the (first) user with username=username as
+    a friend of the current_user.
+    
+    friend user should have sent friend request to current user."""
+@app.post("/addfriend")
+def add_friend (friend_user: Account = Depends(get_user_from_usernameIn),
+                current_user: Account = Depends(get_current_user),
+                db: Session = Depends(get_db)):
+    return send_friend_request(friend_user, current_user, db)
 
 """ Returns a json array of the current user's friends.
 
@@ -232,6 +291,11 @@ def add_friend (friend_user: Account = Depends(get_user_from_usernameIn),
 def get_friends(current_user: Account = Depends(get_current_user), db: Session = Depends(get_db)):
     return [friend_account.toFriendCompressedProfile() for friend_account in current_user.friends]
         
+def get_friend_requests_sent(current_user: Account = Depends(get_current_user), db: Session = Depends(get_db)):
+    return [friend_account.toFriendCompressedProfile() for friend_account in current_user.friend_requests]
+
+def get_friend_requests_recieved(current_user: Account = Depends(get_current_user), db: Session = Depends(get_db)):
+    return [friend_account.toFriendCompressedProfile() for friend_account in current_user.right_friend_requests]
 
 @app.get("/events")
 def get_events(current_user: Annotated[Account, Depends(get_current_user)]):
