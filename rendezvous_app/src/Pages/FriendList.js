7d2368c0e17bb4cf64298ffe6e45847f1b5d6c98
Samuelinto <samuel.mantilla@mail.utoronto.ca>
2024-07-17 17:36:43 -0400

    Implemented design for friends list page

--- rendezvous_app/src/Pages/FriendList.js
+++ rendezvous_app/src/Pages/FriendList.js
@@ -1,19 +1,37 @@
 import React from 'react';
 import Friend from './Friend';
+import './FriendList.css';
+
+import { Link } from 'react-router-dom';
+/** 
+@typedef {Object} FriendProps
+@property {string} username - The username of the friend.
+@property {string} profile_image_src - The URL of the event image.
+*/
+
+
+/* Individual Friend Object
+    Currently includes username and a sample image url. */
+/**@param {FriendProps} props */
+
 
 /* Friend List storing all friends in friends. 
     friends should be a json array with the params
     in Friend. */
 function FriendList({ friends }) {
     return (
-        <div className="friends-list">
-            {friends.map((friend, index) => (
-                <Friend
-                key = {index}
-                {...friend}
-                />
-             ))
-            }    
+        <div className="friends-list-container">
+            <div className='friends-padding'>
+            <div className="friends-container">
+                {friends.map((friend, index) => (
+                    <div className="friend-card" key={index}>
+                        <img src={friend.profile_image_src} alt={friend.name} />
+                        <div className="friend-name">{friend.username}</div>
+                    </div>
+                ))
+                }
+            </div>
+            </div>
         </div>
     );
 };
--- rendezvous_app/src/Pages/friends.js
+++ rendezvous_app/src/Pages/friends.js
@@ -1,64 +1,47 @@
-import React, { useState, useEffect, useRef} from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import BackendApi from './fastapi';
 import { useNavigate } from 'react-router-dom';
-import friends from './friend_data/friends'
-import FriendList from './FriendList'
-
+import friends from './friend_data/friends';
+import FriendList from './FriendList';
+import './friends.css';
 
 /* Friends Page. */
 const Friends = () => {
-    const [friendList, setFriendList] = useState(friends);
-    const navigate = useNavigate();
-    const count = useRef(null);
+  const [friendList, setFriendList] = useState(friends);
+  const navigate = useNavigate();
+  const count = useRef(null);
 
-    /* Database Query on Initial Render
+  /* Database Query on Initial Render
         Friends currently DOES NOT update when another user adds
         the current user as a friend */
-    useEffect(() => {
-        if(count.current == null){
-            BackendApi.post('/friends').then((response) => {setFriendList(response.data);});
-        }
-        return () => {count.current = 1; }
-    }, []);
-    
-
-    const addFriendBtn = {
-        margin: "20px 0",
-        justifyContent: "center",
-        alignItems: "center",
-        padding: "10px",
-        backgroundColor: "green",
-        color: "white",
-        border: "none",
-        borderRadius: "5px",
-        cursor: "pointer",
+  useEffect(() => {
+    if (count.current == null) {
+      BackendApi.post('/friends').then((response) => {
+        setFriendList(response.data);
+      });
+    }
+    return () => {
+      count.current = 1;
     };
+  }, []);
 
-    const handleAddFriendClick = () => {
-        navigate('/addfriend');
-    };
+  const handleAddFriendClick = () => {
+    navigate('/addfriend');
+  };
 
-    return (
-        <div
-            style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                marginTop: "20px",
-                flexDirection: "column",
-            }}
-        >
-            <h1>Friends List</h1>
-
-            <div style={{ marginBottom: "20px" }}>
-                <button style={addFriendBtn} onClick={handleAddFriendClick}>
-                    <h3>+ Add Friend</h3>
-                </button>
-            </div>
-            <FriendList friends={friendList} />
+  return (
+    <div>
+      <div className='centerstyle'>
+        <h1 className='Friends-list-header'>My Friends</h1>
+        <div style={{ marginBottom: '20px' }}>
+          <button className='addFriendBtn' onClick={handleAddFriendClick}>
+            <h3>+ Add Friend</h3>
+          </button>
         </div>
-
-    );
+      </div>
+      <FriendList friends={friendList} />
+    </div>
+  );
 };
 
 export default Friends;
